9c8e78438b59e6ed457df004f7f28a65d5732121
Akshat <akshatpal18@gmail.com>
2022-11-18 02:30:17 +0530

    v2envedit

--- env.js
+++ env.js
@@ -2,8 +2,8 @@ let env = {
   SERVER_IP: "15.207.96.150",
   DB_PORT: "3306",
   DB_USER: "admin",
-  DB_PASS: "root",
-  DB_NAME: `y0y0FU98`,
+  DB_PASS: "y0y0FU98",
+  DB_NAME: `nodelogin`,
 
   //'IPINFO_SERVER_URL': https://ipinfo.io
 };
