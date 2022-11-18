2549f8bdeff44d18d510e38e9a8aff639fe250bc
Akshat <akshatpal18@gmail.com>
2022-11-19 01:51:57 +0530

    securitycheck

---
+++ env.js
@@ -0,0 +1,14 @@
+let env = {
+  SERVER: "*",
+  PORT:"3001",
+  PROTOCOL:"http://",
+  DB_SERVER: "1*",
+  DB_PORT: "3306",
+  DB_USER: "*",
+  DB_PASS: "*",
+  DB_NAME: `*`,
+
+  //'IPINFO_SERVER_URL': https://ipinfo.io
+};
+
+module.exports = env
